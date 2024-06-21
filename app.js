2cad15c7ffdd8448d37465db16e3220f599b110f
lawrencejr5 <oputalawrence@gmail.com>
2024-06-21 03:27:23 +0100

    deployment process

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+
+// Security packages
 const cors = require("cors");
+const xss = require("xss-clean");
+const helmet = require("helmet");
+const rateLimiter = require("express-rate-limit");
 
 const ConnectDb = require("./db/conn");
 const jobsRouter = require("./routes/jobs");
@@ -11,9 +16,18 @@ const auth = require("./middlewares/auth");
 
 const notFound = require("./middlewares/not-found");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
-app.use(cors());
 app.use(express.static("./public"));
+app.use(cors());
+app.use(helmet());
+app.use(xss());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", auth, jobsRouter);
